18b94c0d373c365c51fefeddec3c3aea84f26384
Diego Patino <dipaco@gmail.com>
2018-10-18 15:52:20 -0400

    Data generation script updated.

--- utils/data_prep_chordiogram.py
+++ utils/data_prep_chordiogram.py
@@ -52,39 +52,6 @@ def cartesian2spherical(x, y, z, r=None):
     return r, theta, phi
 
 
-def _uniform_sample_chords(mesh, num_samples):
-
-
-    num_sample_faces = num_samples
-    if mesh.faces.shape[0] < num_sample_faces:
-        sample_faces = mesh.faces
-        num_sample_faces = mesh.faces.shape[0]
-        normals = -mesh.face_normals
-    else:
-        idx = np.arange(mesh.faces.shape[0])
-        np.random.shuffle(idx)
-        sample_faces = mesh.faces[idx[:num_sample_faces], :]
-        normals = -mesh.face_normals[idx[:num_sample_faces], :]
-    point1 = mesh.vertices[sample_faces[:, 0], :]
-    point2 = mesh.vertices[sample_faces[:, 1], :]
-    point3 = mesh.vertices[sample_faces[:, 2], :]
-    c = (point1 + point2 + point3) / 3.0
-    m_x = np.dot(c[:, 0][:, None], np.ones((1, num_sample_faces))) - np.dot(np.ones((num_sample_faces, 1)),
-                                                                            c[:, 0][None])
-    m_y = np.dot(c[:, 1][:, None], np.ones((1, num_sample_faces))) - np.dot(np.ones((num_sample_faces, 1)),
-                                                                            c[:, 1][None])
-    m_z = np.dot(c[:, 2][:, None], np.ones((1, num_sample_faces))) - np.dot(np.ones((num_sample_faces, 1)),
-                                                                            c[:, 2][None])
-    m = np.stack((m_x, m_y, m_z), axis=2)
-    n_x = np.dot(np.ones((num_sample_faces, 1)), normals[:, 0][None])
-    n_y = np.dot(np.ones((num_sample_faces, 1)), normals[:, 1][None])
-    n_z = np.dot(np.ones((num_sample_faces, 1)), normals[:, 2][None])
-    n_p = np.stack((n_x, n_y, n_z), axis=2)
-    n_q = np.transpose(n_p, (1, 0, 2))
-
-    return m, n_p, n_q, num_sample_faces
-
-
 def _sample_chords(mesh, num_samples, sampling_method='random'):
 
     if num_samples > mesh.faces.shape[0]**2:
@@ -206,14 +173,10 @@ def get_chords3(mesh, num_samples=100, sampling_method='uniform'):
     return chrs
 
 
-METHODS = {1: {'name': '6_angles', 'num_features': 7, 'function': get_chords1},
-           2: {'name': '2_angles', 'num_features': 3, 'function': get_chords2},
-           3: {'name': 'angle_bt_planes', 'num_features': 4, 'function': get_chords3}
+METHODS = {1: {'name': 'plane0', 'num_features': 7, 'function': get_chords1},
+           2: {'name': 'plane1', 'num_features': 3, 'function': get_chords2},
+           3: {'name': 'plane2', 'num_features': 4, 'function': get_chords3}
            }
-
-SAMPLING_METHODS = {'random': _uniform_sample_chords,
-                    'area_weighted': _sample_chords
-                    }
 
 
 def create_chordiogram_h5(file_num, batch_size, classes, paths, output_path, sampling_method, num_samples=100,
@@ -230,8 +193,6 @@ def create_chordiogram_h5(file_num, batch_size, classes, paths, output_path, sam
         mesh = trimesh.load(f)
         mesh.remove_degenerate_faces()
         mesh.remove_duplicate_faces()
-        mesh = trimesh.permutate.Permutator().permutate(mesh)
-        mesh = trimesh.permutate.Permutator(mesh)
         mesh.vertices -= mesh.centroid
         mesh.vertices /= np.linalg.norm(mesh.vertices, axis=1).max()
 
@@ -248,15 +209,6 @@ def create_chordiogram_h5(file_num, batch_size, classes, paths, output_path, sam
     f.close()
 
 
-def read_checkpoint(output_path):
-    checkpoints_folder = os.path.join(output_path, 'checkpoints')
-    if not os.path.exists(checkpoints_folder):
-        os.makedirs(checkpoints_folder)
-    file = open(os.path.join(checkpoints_folder, '{}.txt').format(filenumber), 'w')
-    file.write('done.')
-    file.close()
-
-
 if __name__ == '__main__':
 
     sampling_methods = ['random', 'area_weighted']
@@ -264,12 +216,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-d', '--input', help='Input Modelnet folder', dest='input_dataset', default='.')
-    parser.add_argument('-b', '--batch_size', help="Batch size for each h5 file", dest="batch_size", type=int)
+    parser.add_argument('-b', '--batch_size', help="Batch size for each h5 file", dest="batch_size", type=int, default=2048)
     parser.add_argument('-m', '--method', help="Method to sample the point cloud", dest="sampling_method",
                         choices=sampling_methods, default='random')
     parser.add_argument('-o', '--output', help="Output folder", dest='output_dataset')
     parser.add_argument('-c', '--chord_type', help="Chord feature type", dest='chord_type', default=1, type=int)
-    parser.add_argument('-n', '--num_samples', help="num of random samples", dest='num_samples', default=10000)
+    parser.add_argument('-n', '--num_samples', help="num of random samples", dest='num_samples', default=2048, type=int)
     args = parser.parse_args()
 
     if not os.path.exists(args.output_dataset):
@@ -294,9 +246,9 @@ if __name__ == '__main__':
 
     num_h5_train = int(np.ceil(len(all_train_paths) / args.batch_size))
     d = args.batch_size
-    for i in range(num_h5_train):
-       create_chordiogram_h5(i, d, classes, all_train_paths[i * d:(i + 1) * d], args.output_dataset,
-                             args.sampling_method, args.num_samples, args.chord_type, 'train')
+    #for i in range(num_h5_train):
+    #   create_chordiogram_h5(i, d, classes, all_train_paths[i * d:(i + 1) * d], args.output_dataset,
+    #                         args.sampling_method, args.num_samples, args.chord_type, 'train')
 
     num_h5_train = int(np.ceil(len(all_train_paths) / args.batch_size))
     d = args.batch_size
