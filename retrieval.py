e88c548e1ec9e869e4fd8079d3c830f119ff57a0
Diego Patino <dipaco@gmail.com>
2018-11-13 20:24:29 -0500

    Adapting retrieval to the new scheme in options.py

--- retrieval.py
+++ retrieval.py
@@ -1,134 +1,71 @@
-import tensorflow as tf
-import numpy as np
+import os
+import sys
 import argparse
 import socket
 import importlib
 import time
-import os
 import scipy.misc
-import sys
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(BASE_DIR)
-sys.path.append(os.path.join(BASE_DIR, 'models'))
-sys.path.append(os.path.join(BASE_DIR, 'utils'))
-from scipy.spatial.distance import pdist, squareform
+import subprocess
+
+import tensorflow as tf
+import numpy as np
+
+import options
+import provider
+
+from utils.util import perturb_data
+from utils.util import log_string
 from joblib import Parallel, delayed
 from pathlib import Path
+from scipy.spatial.distance import pdist, squareform
 from sklearn.metrics import precision_recall_curve, precision_score
-import provider
-import subprocess
-# import pc_util
+
+FLAGS = options.get_options()
+
+SHAPE_NAMES = [line.rstrip() for line in open(FLAGS.shape_names_path)]
+TRAIN_FILES = provider.getDataFiles(FLAGS.train_path)
+TEST_FILES = provider.getDataFiles(FLAGS.test_path)
 
 
-model_choices = ["pointnet_cls", "pointnet_cls_basic", "pointnet_no3trans", "pointnet_notrans"]
-dataset_choices = ["plane0", "plane1", "plane2", "original", "darboux"]
-train_test = ["z-z", "z-so3", "so3-so3"]
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--gpu', type=int, default=0, help='GPU to use [default: GPU 0]')
-parser.add_argument('--model', default='pointnet_cls', choices=model_choices, help='Model name: pointnet_cls or pointnet_cls_basic [default: pointnet_cls]')
-parser.add_argument('--dataset', default='plane1', choices=dataset_choices, help='Dataset: chordiogram representation [default: plane11]')
-parser.add_argument('--log_dir', default='log', help='Log dir [default: log]')
-parser.add_argument('--num_point', type=int, default=1024, help='Point Number [256/512/1024/2048] [default: 1024]')
-parser.add_argument('--max_epoch', type=int, default=250, help='Epoch to run [default: 250]')
-parser.add_argument('--batch_size', type=int, default=32, help='Batch Size during training [default: 32]')
-parser.add_argument('--train_test', default="z-z", help='Decay rate for lr decay [default: z-z]')
-parser.add_argument('--model_path', default='log/model.ckpt', help='model checkpoint file path [default: log/model.ckpt]')
-parser.add_argument('--dump_dir', default='dump', help='dump folder path [dump]')
-parser.add_argument('--visu', action='store_true', help='Whether to dump image for error case [default: False]')
-FLAGS = parser.parse_args()
+# def log_string(out_str):
+#     FLAGS.log_file.write(out_str+'\n')
+#     FLAGS.log_file.flush()
+#     print(out_str)
 
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--gpu', type=int, default=0, help='GPU to use [default: GPU 0]')
-# parser.add_argument('--model', default='pointnet_cls', help='Model name: pointnet_cls or pointnet_cls_basic [default: pointnet_cls]')
-# parser.add_argument('--batch_size', type=int, default=4, help='Batch Size during training [default: 1]')
-# parser.add_argument('--num_point', type=int, default=1024, help='Point Number [256/512/1024/2048] [default: 1024]')
-# parser.add_argument('--model_path', default='log/model.ckpt', help='model checkpoint file path [default: log/model.ckpt]')
-# parser.add_argument('--dump_dir', default='dump', help='dump folder path [dump]')
-# parser.add_argument('--visu', action='store_true', help='Whether to dump image for error case [default: False]')
-# FLAGS = parser.parse_args()
+# def perturb_data(FLAGS, data, mode='train'):
+#     if FLAGS.dataset in ["original"]:
+#         rotated_data = provider.rotate_point_cloud(data, mode, FLAGS.train_test)
+#         jittered_data = provider.jitter_point_cloud(rotated_data)
+#     elif FLAGS.dataset in ["plane0"]:
+#         rotated_data = provider.rotate_plane0_point_cloud(data, mode, FLAGS.train_test)
+#         jittered_data = provider.jitter_plane0(rotated_data)
+#     elif FLAGS.dataset in ["darboux_expand"]:
+#         rotated_data = provider.expand_darboux(data)
+#         jittered_data = provider.jitter_expand_darboux(rotated_data)
+#     else:
+#         rotated_data = data
+#         if FLAGS.dataset in ["plane1"]:
+#             jittered_data = provider.jitter_plane1(rotated_data)
+#         elif FLAGS.dataset in ["plane2"]:
+#             jittered_data = provider.jitter_plane2(rotated_data)
+#         else:
+#             jittered_data = provider.jitter_darboux(rotated_data)
 
-
-BATCH_SIZE = FLAGS.batch_size
-NUM_POINT = FLAGS.num_point
-MODEL_PATH = FLAGS.model_path
-GPU_INDEX = FLAGS.gpu
-MODEL = importlib.import_module(FLAGS.model) # import network module
-TRAIN_TEST = FLAGS.train_test
-DUMP_DIR = FLAGS.dump_dir
-LOG_DIR = FLAGS.log_dir
-if not os.path.exists(DUMP_DIR): os.mkdir(DUMP_DIR)
-LOG_FOUT = open(os.path.join(DUMP_DIR, 'log_evaluate.txt'), 'w')
-LOG_FOUT.write(str(FLAGS)+'\n')
-
-NUM_CLASSES = 40
-SHAPE_NAMES = [line.rstrip() for line in \
-    open(os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048/shape_names.txt'))] 
-
-HOSTNAME = socket.gethostname()
-
-# ModelNet40 official train/test split
-# TRAIN_FILES = provider.getDataFiles( \
-#     os.path.join(BASE_DIR, '/NAS/data/christine/modelnet40_ply_hdf5_2048/train_files.txt'))
-# TEST_FILES = provider.getDataFiles(\
-#     os.path.join(BASE_DIR, '/NAS/data/christine/modelnet40_ply_hdf5_2048/test_files.txt'))
-
-DatasetPath = {
-    "plane0": {
-        "train": os.path.join(BASE_DIR, '/NAS/data/diego/chords_dataset/plane0/train_files.txt'),
-        "test": os.path.join(BASE_DIR, '/NAS/data/diego/chords_dataset/plane0/test_files.txt'),
-        "num_chord_features": 7,
-    },
-    "plane1": {
-        "train": os.path.join(BASE_DIR, '/NAS/data/diego/chords_dataset/plane1/train_files.txt'),
-        "test": os.path.join(BASE_DIR, '/NAS/data/diego/chords_dataset/plane1/test_files.txt'),
-        "num_chord_features": 3,
-    },
-    "plane2": {
-        "train": os.path.join(BASE_DIR, '/NAS/data/diego/chords_dataset/plane2/train_files.txt'),
-        "test": os.path.join(BASE_DIR, '/NAS/data/diego/chords_dataset/plane2/test_files.txt'),
-        "num_chord_features": 4,
-    },
-    "original": {
-        "train": os.path.join(BASE_DIR, '/NAS/data/christine/modelnet40_ply_hdf5_2048/train_files.txt'),
-        "test": os.path.join(BASE_DIR, '/NAS/data/christine/modelnet40_ply_hdf5_2048/test_files.txt'),
-        "num_chord_features": 3,
-    },
-    "darboux": {
-        "train": os.path.join(BASE_DIR, '/Users/dipaco/Documents/Datasets/chord_point_net/darboux/train_files.txt'),
-        "test": os.path.join(BASE_DIR, '/Users/dipaco/Documents/Datasets/chord_point_net/darboux/test_files.txt'),
-        "num_chord_features": 4,
-    },
-}
-
-DSET_INFO = DatasetPath[FLAGS.dataset]
-#TRAIN_FILES = provider.getDataFiles( \
-#    os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048/train_files.txt'))
-#TEST_FILES = provider.getDataFiles(\
-#    os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048/test_files.txt'))
-TRAIN_FILES = provider.getDataFiles(DSET_INFO['train'])
-    # os.path.join(BASE_DIR, '../../data/chords_dataset/train_files_2_angles.txt'))
-
-TEST_FILES = provider.getDataFiles(DSET_INFO['test'])
-    # os.path.join(BASE_DIR, '../../data/chords_dataset/test_files_2_angles.txt'))
-
-
-def log_string(out_str):
-    LOG_FOUT.write(out_str+'\n')
-    LOG_FOUT.flush()
-    print(out_str)
+#     return rotated_data, jittered_data
 
 def retrieval():
     is_training = False
      
-    with tf.device('/gpu:'+str(GPU_INDEX)):
-        pointclouds_pl, labels_pl = MODEL.placeholder_inputs(BATCH_SIZE, NUM_POINT, DSET_INFO['num_chord_features'])
+    with tf.device('/gpu:'+str(FLAGS.gpu)):
+        pointclouds_pl, labels_pl = FLAGS.model.placeholder_inputs(FLAGS.FLAGS.batch_size, FLAGS.num_point,
+                                                                   FLAGS.num_chord_features)
         is_training_pl = tf.placeholder(tf.bool, shape=())
 
         # simple model
-        pred, end_points, feature_map = MODEL.get_model(pointclouds_pl, is_training_pl, input_dims=DSET_INFO['num_chord_features'], return_feature_map=True)
-        loss = MODEL.get_loss(pred, labels_pl, end_points)
+        pred, end_points, feature_map = FLAGS.model.get_model(pointclouds_pl, is_training_pl, input_dims=FLAGS.num_chord_features,
+                                                 num_classes=FLAGS.num_classes, return_feature_map=True)
+        loss = FLAGS.model.get_loss(pred, labels_pl, end_points)
         
         # Add ops to save and restore all the variables.
         saver = tf.train.Saver()
@@ -138,10 +75,10 @@ def retrieval():
     config.gpu_options.allow_growth = True
     config.allow_soft_placement = True
     config.log_device_placement = True
-    sess = tf.Session(config=config)
+    sess = tf.Session(config=FLAGS.config)
 
     # Restore variables from disk.
-    saver.restore(sess, MODEL_PATH)
+    saver.restore(sess, FLAGS.FLAGS.model_path)
     log_string("Model restored.")
 
     ops = {'pointclouds_pl': pointclouds_pl,
@@ -160,9 +97,9 @@ def retrieval_one_epoch(sess, ops, num_votes=1, topk=1):
     total_correct = 0
     total_seen = 0
     loss_sum = 0
-    total_seen_class = [0 for _ in range(NUM_CLASSES)]
-    total_correct_class = [0 for _ in range(NUM_CLASSES)]
-    fout = open(os.path.join(DUMP_DIR, 'pred_label.txt'), 'w')
+    total_seen_class = [0 for _ in range(FLAGS.num_classes)]
+    total_correct_class = [0 for _ in range(FLAGS.num_classes)]
+    fout = open(os.path.join(FLAGS.dump_dir, 'pred_label.txt'), 'w')
     all_descriptors = np.array([[]])
     scores = np.array([[]])
     labels = np.array([[]])
@@ -170,25 +107,20 @@ def retrieval_one_epoch(sess, ops, num_votes=1, topk=1):
     for fn in range(len(TEST_FILES)):
         log_string('----'+str(fn)+'----')
         current_data, current_label, current_fnames = provider.loadDataFile(TEST_FILES[fn], return_fnames=True)
-        current_data = current_data[:, 0:NUM_POINT, :]
+        current_data = current_data[:, 0:FLAGS.num_point, :]
         current_label = np.squeeze(current_label)
         print(current_data.shape)
         
         file_size = current_data.shape[0]
-        num_batches = file_size // BATCH_SIZE
+        num_batches = file_size // FLAGS.batch_size
         print(file_size)
         
         for batch_idx in range(num_batches):
-            start_idx = batch_idx * BATCH_SIZE
-            end_idx = (batch_idx+1) * BATCH_SIZE
+            start_idx = batch_idx * FLAGS.batch_size
+            end_idx = (batch_idx+1) * FLAGS.batch_size
             cur_batch_size = end_idx - start_idx
 
-            if FLAGS.dataset in ["original"]:
-                rotated_data = provider.rotate_point_cloud(current_data[start_idx:end_idx, :, :], 'test', TRAIN_TEST)
-            elif FLAGS.dataset in ["plane0"]:
-                rotated_data = provider.rotate_plane0_point_cloud(current_data[start_idx:end_idx, :, :], 'test', TRAIN_TEST)
-            else:
-                rotated_data = current_data[start_idx:end_idx, :, :]
+            rotated_data, _ = perturb_data(FLAGS, current_data[start_idx:end_idx, :, :], 'test')
 
             feed_dict = {ops['pointclouds_pl']: rotated_data,
                          ops['labels_pl']: current_label[start_idx:end_idx],
@@ -207,15 +139,15 @@ def retrieval_one_epoch(sess, ops, num_votes=1, topk=1):
                 labels = current_label[start_idx:end_idx]
                 fnames = current_fnames[start_idx:end_idx]
 
-    fname = MODEL_PATH + '_dists.npy'
+    fname = FLAGS.model_path + '_dists.npy'
     dists = compute_and_save_descriptors_dists(all_descriptors, fname)
 
     thresh = search_thresholds(dists, labels)
 
-    make_shrec17_output_thresh(all_descriptors, scores, fnames, MODEL_PATH,
+    make_shrec17_output_thresh(all_descriptors, scores, fnames, FLAGS.model_path,
                                distance='cosine', dists=dists, thresh=thresh)
 
-    res = eval_shrec17_output(os.path.split(LOG_DIR)[0])
+    res = eval_shrec17_output(os.path.split(FLAGS.log_dir)[0])
     #print(modeldir, datadir, ckpt)
     print(res.head(1))
     print(res.tail(1))
@@ -329,4 +261,4 @@ def eval_shrec17_output(outdir):
 if __name__ == '__main__':
     with tf.Graph().as_default():
         retrieval()
-    LOG_FOUT.close()
+    FLAGS.log_file.close()
