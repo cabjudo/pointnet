636fe8a3c880968ad42c5f87196ca747c608ae59
Diego Patino <dipaco@gmail.com>
2018-11-14 21:36:37 -0500

    Bugfix: removing extra ) in the architectures

--- models/pointnet_notrans_add3x64.py
+++ models/pointnet_notrans_add3x64.py
@@ -100,7 +100,7 @@ def get_loss(pred, label, end_points, reg_weight=0.001):
 def get_trip_loss(pred, label, features, reg_weight=0.001):
     """ pred: B*NUM_CLASSES,
         label: B, """
-    features = tf.math.l2_normalize(features, axis=-1))
+    features = tf.math.l2_normalize(features, axis=-1)
     trip_loss = tf.contrib.losses.metric_learning.triplet_semihard_loss(labels=label, embeddings=features)
     tf.summary.scalar('triplet loss', trip_loss)
 
